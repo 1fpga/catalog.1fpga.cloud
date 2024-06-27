68c22132949cc4d08775cdc5002779de3da3d333
Hans Larsen <hans@larsen.online>
2024-06-27 15:38:53 -0700

    Add DAT files and golem.json and a bunch of other things

--- build/build.js
+++ build/build.js
@@ -3,19 +3,21 @@ import * as glob from 'glob';
 import * as path from 'path';
 import * as toml from 'toml';
 
-// Convert the toml files to json.
-glob.globSync('../json/**/*.toml').forEach(file => {
-    const destDir = path.dirname(file.replace('../json/', '../dist/json/'));
-    fs.mkdirSync(destDir, { recursive: true });
-
-    const destPath = path.join(destDir, path.basename(file, '.toml') + '.json');
-    const json = JSON.stringify(toml.parse(fs.readFileSync(file, 'utf8')));
-    fs.writeFileSync(destPath, json);
-});
-
 // Copy files.
 glob.globSync('../files/**', { nodir: true }).forEach(file => {
     const dest = path.dirname(file.replace('../files/', '../dist/'));
-    fs.mkdirSync(dest, { recursive: true });
-    fs.copyFileSync(file, path.join(dest, path.basename(file)));
+
+    // Convert toml files to json.
+    if (path.extname(file) === '.toml') {
+        const destPath = path.join(destDir, path.basename(file, '.toml') + '.json');
+        const json = JSON.stringify(toml.parse(fs.readFileSync(file, 'utf8')));
+
+        fs.mkdirSync(dest, { recursive: true });
+        fs.writeFileSync(destPath, json);
+    } else if (path.basename(file) === 'README.md') {
+        // Ignore README.md files.
+    } else {
+        fs.mkdirSync(dest, { recursive: true });
+        fs.copyFileSync(file, path.join(dest, path.basename(file)));
+    }
 });
