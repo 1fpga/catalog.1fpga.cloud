f2a10f0813d33b2758c81ba6061bb1452640bdec
Hans Larsen <hans@larsen.online>
2024-06-27 15:43:34 -0700

    Fix build script

--- build/build.js
+++ build/build.js
@@ -5,19 +5,17 @@ import * as toml from 'toml';
 
 // Copy files.
 glob.globSync('../files/**', { nodir: true }).forEach(file => {
-    const dest = path.dirname(file.replace('../files/', '../dist/'));
+    const destDir = path.dirname(file.replace('../files/', '../dist/'));
+    fs.mkdirSync(destDir, { recursive: true });
 
     // Convert toml files to json.
     if (path.extname(file) === '.toml') {
         const destPath = path.join(destDir, path.basename(file, '.toml') + '.json');
         const json = JSON.stringify(toml.parse(fs.readFileSync(file, 'utf8')));
-
-        fs.mkdirSync(dest, { recursive: true });
         fs.writeFileSync(destPath, json);
     } else if (path.basename(file) === 'README.md') {
         // Ignore README.md files.
     } else {
-        fs.mkdirSync(dest, { recursive: true });
-        fs.copyFileSync(file, path.join(dest, path.basename(file)));
+        fs.copyFileSync(file, path.join(destDir, path.basename(file)));
     }
 });
