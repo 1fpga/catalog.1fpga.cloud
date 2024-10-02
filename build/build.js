ed8a63e602f25240c91b2cbc2e86216be0c3b49c
Hans Larsen <hans@larsen.online>
2024-10-02 11:34:32 -0700

    New nes.json and compile size and shas at build time

--- build/build.js
+++ build/build.js
@@ -2,6 +2,7 @@ import * as fs from 'fs';
 import * as glob from 'glob';
 import * as path from 'path';
 import * as toml from 'toml';
+import * as crypto from 'crypto';
 
 // Copy files.
 glob.globSync('../files/**', { nodir: true }).forEach(file => {
@@ -24,3 +25,45 @@ glob.globSync('../files/**', { nodir: true }).forEach(file => {
         fs.copyFileSync(file, path.join(destDir, path.basename(file)));
     }
 });
+
+// Update sha1sum and sizes.
+function calculateSizeAndSha1(path) {
+    const stat = fs.statSync(path);
+    const size = stat.size;
+    const sha1 = crypto.createHash('sha1').update(fs.readFileSync(path)).digest('hex');
+    return [size, sha1];
+}
+
+const catalogPath = path.join(process.cwd(),'../dist/catalog.json');
+const catalog = JSON.parse(fs.readFileSync(catalogPath, 'utf8'));
+for (const s of Object.values(catalog.systems)) {
+    const sPath = path.join(path.dirname(catalogPath), s.url);
+    const sData = JSON.parse(fs.readFileSync(sPath, 'utf8'));
+
+    // Update the gamesDb of the system.
+    if (sData.gamesDb) {
+        const gamesDbPath = path.join(path.dirname(sPath), sData.gamesDb.url);
+        const [size, sha1] = calculateSizeAndSha1(gamesDbPath);
+        sData.gamesDb.size = size;
+        sData.gamesDb.sha1 = sha1;
+    }
+
+    fs.writeFileSync(sPath, JSON.stringify(sData), 'utf8');
+
+    // Update the cores' releases' files size and sha1.
+    for (const c of Object.values(sData.cores)) {
+        const cPath = path.join(path.dirname(sPath), c.url);
+        const cData = JSON.parse(fs.readFileSync(cPath, 'utf8'));
+
+        for (const r of cData.releases) {
+            for (const f of r.files) {
+                const fPath = path.join(path.dirname(cPath), f.url);
+                const [size, sha1] = calculateSizeAndSha1(fPath);
+                f.size = size;
+                f.sha1 = sha1;
+            }
+        }
+
+        fs.writeFileSync(cPath, JSON.stringify(cData), 'utf8');
+    }
+}
